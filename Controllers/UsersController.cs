03ca033bdb9d5f0f290a6c44ea280e8bf3920c54
Oleg <oleg.davydov@walletfactory.com>
2019-11-24 23:56:42 +0200

    first

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+       
+    {
+        AppDataBaseContext db;
+        public UsersController(AppDataBaseContext context)
+        {
+            this.db = context;
+
+
+                db.Users.Add(new User { Name = "bob", Age = 66, Phone = +3989879877 });
+                db.Users.Add(new User { Name = "kolya", Age = 26, Phone = +39464665478 });
+                db.Users.Add(new User { Name = "vasya", Age = 31, Phone = +394545455 });
+                db.Users.Add(new User { Name = "ola", Age = 26, Phone = +3945454533 });
+                db.Users.Add(new User { Name = "max", Age = 8, Phone = +345458794552 });
+
+                db.SaveChanges();
+            
+        }
+        
+        // GET: api/Users
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "some user";
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace Users
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<AppDataBaseContext>
+                (o => o.UseSqlServer("Data Source=localhost;Initial Catalog=AppDataBaseContext;Integrated Security=True"));
+            services.AddSpaStaticFiles(options => options.RootPath = "client-app/dist");
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "client-app";
+                if (env.IsDevelopment())
+                {
+                    spa.UseVueDevelopmentServer();
+                }
+            });
+        }
+    }
+}
